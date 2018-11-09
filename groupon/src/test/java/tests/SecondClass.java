0b3892755e941db28eaea05c9ee07e7e534c137e
Hemanth <hemaanth01@gmail.com>
2018-11-08 20:38:20 -0600

    test

--- groupon/src/test/java/tests/SecondClass.java
+++ groupon/src/test/java/tests/SecondClass.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
 
 import common.BaseTest;
@@ -24,13 +25,20 @@ public class SecondClass extends BaseTest {
 		driver = setup.browserSetup(driver);
 		calculatePage = new CalculatorPage(driver);
 		System.out.println(excel.getData("PARAM1"));
-		calculatePage.printAllElements();
-		driver.quit();
-		/*
-		 * driver.findElement(By.name("userName")).sendKeys("Demo");
-		 * driver.findElement(By.name("password")).sendKeys("demo");
-		 * driver.findElement(By.name("login")).click();
-		 */
+		//calculatePage.printAllElements();
+		
+		
+		  driver.findElement(By.name("userName")).sendKeys("Demo");
+		  driver.findElement(By.name("password")).sendKeys("demo");
+		  driver.findElement(By.name("login")).click();
+		 
+		  WebElement sel = driver.findElement(By.name("passCount"));
+		  
+		  Select select = new Select(sel);
+		  String selVal = excel.getData("PARAM5");
+		  select.selectByIndex(Integer.parseInt(selVal));
+		  
+		  driver.quit();		  
 
 		// driver.findElement(By.linkText("REGISTER")).click();
 		// driver.findElement(By.partialLinkText("Hote")).click();;
